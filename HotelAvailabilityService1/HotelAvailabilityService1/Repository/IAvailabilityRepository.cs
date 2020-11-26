b56d17f5fc157fcd42c987cf7cf63ec9c600d09f
ChinthapalliNikhithaChandana <chnikhitha99@gmail.com>
2020-11-26 21:13:06 +0530

    first commit

---
+++ HotelAvailabilityService1/HotelAvailabilityService1/Repository/IAvailabilityRepository.cs
@@ -0,0 +1,16 @@
+﻿using HotelAvailabilityService1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelAvailabilityService1.Repository
+{
+    public interface IAvailabilityRepository
+    {
+        public AvailableHotels GetById(int id);
+        public IEnumerable<AvailableHotels> GetAll();
+        public bool Reduce(int id);
+        public AvailableHotels AddHotel(AvailableHotels hotel);
+    }
+}
